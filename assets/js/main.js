430332a5ad14b3c93a09e8635f91c815c7946312
Giovanni Colucci <giovanni.colucci@neom.com>
2025-04-26 00:00:41 +0200

    Revert "rtrt"
    This reverts commit 23524c50888b4a09033521433e14ddf7bb77d2ed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  (event) => {
-    particlesJS.load("particles-js", "assets/js/particles.json", function () {
-      console.log("particles.js loaded - callback");
+window.onload = function () {
+    Particles.init({
+        selector: '.background',
+        color: ['#f8f9fa', '#ced4da', '#868e96', '#74c0fc', '#4dabf7']
     });
-  },
-  false
-);
+};
